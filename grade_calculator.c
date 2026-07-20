89ba90fbb30cafd4ecc41276c5c9ea1f1a397e1d
Shrishti <shrishti8k@gmail.com>
2026-07-20 21:45:01 +0530

    Add grade calculator program

--- grade_calculator.c
+++ grade_calculator.c
@@ -2,28 +2,45 @@
 
 int main() {
     int marks;
-    printf("Enter marks: ");
+
+    printf("\n===== Grade Calculator =====\n\n");
+
+    printf("Enter marks : ");
     scanf("%d", &marks);
 
-    if(marks >= 91 && marks <= 100) {
-        printf("A1");
-    } else if(marks >= 81 && marks <= 90) {
-        printf("A2");
-    } else if(marks >= 71 && marks <= 80) {
-        printf("B1");
-    } else if(marks >= 61 && marks <= 70) {
-        printf("B2");
-    } else if(marks >= 51 && marks <= 60) {
-        printf("C1");
-    } else if(marks >= 41 && marks <= 50) {
-        printf("C2");
-    } else if(marks >= 33 && marks <= 40) {
-        printf("D");
-    } else if(marks >= 0 && marks <= 32) {
-        printf("E");
+    printf("\n-----------Result-----------\n");
+    printf("Marks      : %d\n", marks);
+    if(marks < 0 || marks > 100) {
+        printf("Invalid marks\n");
     } else {
-        printf("Invalid marks");
+        printf("Percentage : %d%%\n", marks);
+
+        if(marks >= 91 && marks <= 100) {
+            printf("Grade      : A1\n");
+        } else if(marks >= 81 && marks <= 90) {
+            printf("Grade      : A2\n");
+        } else if(marks >= 71 && marks <= 80) {
+            printf("Grade      : B1\n");
+        } else if(marks >= 61 && marks <= 70) {
+            printf("Grade      : B2\n");
+        } else if(marks >= 51 && marks <= 60) {
+            printf("Grade      : C1\n");
+        } else if(marks >= 41 && marks <= 50) {
+            printf("Grade      : C2\n");
+        } else if(marks >= 33 && marks <= 40) {
+            printf("Grade      : D\n");
+        } else {
+            printf("Grade      : E\n");
+        }
+
+            
+        if(marks >= 33) {
+            printf("Result     : PASS\n");
+        } else {
+            printf("Result     : FAIL\n");
+        }
     }
 
+    printf("----------------------------");
     return 0;
 }
